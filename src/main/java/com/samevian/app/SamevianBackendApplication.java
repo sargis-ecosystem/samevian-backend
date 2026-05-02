7206427bbf65d8dc83b6ad2c3a00aa67ff8256a1
messaqo <messaqo@samevian.local>
2026-05-02 19:49:57 +0400

    Fix scanBasePackages

--- src/main/java/com/samevian/app/SamevianBackendApplication.java
+++ src/main/java/com/samevian/app/SamevianBackendApplication.java
@@ -3,7 +3,7 @@ package com.samevian.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.samevian")
 public class SamevianBackendApplication {
 
 	public static void main(String[] args) {
